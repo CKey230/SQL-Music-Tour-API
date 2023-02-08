cc2245532ac60a690f024a3eccc92abbeb1c846f
Abigail Jones <ckey230@gmail.com>
2023-02-07 20:23:06 -0500

    association

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -3,7 +3,7 @@ const db = require('../models')
 const { Event } = db 
 const { Op } = require('sequelize')
 
-//all events
+// find all events 
 events.get('/', async (req, res) => {
     try {
         const foundEvents = await Event.findAll({
@@ -18,62 +18,62 @@ events.get('/', async (req, res) => {
     }
 })
 
-//event by ID
-events.get('/:id', async (req,res) => {
-    try{
-        const foundEvents = await Event.findOne({
+// search for event
+events.get('/:id', async (req, res) => {
+    try {
+        const foundEvent = await Event.findOne({
             where: { event_id: req.params.id }
         })
-        res.status(200).json(foundEvents)
+        res.status(200).json(foundEvent)
     } catch (error) {
         res.status(500).json(error)
     }
 })
 
-
-//create event
-events.post('/', async (req,res) => {
+//create event 
+events.post('/', async (req, res) => {
     try {
         const newEvent = await Event.create(req.body)
         res.status(200).json({
-            message: 'Successfully created a new event',
+            message: 'Successfully inserted a new event',
             data: newEvent
         })
-    } catch (err) {
+    } catch(err) {
         res.status(500).json(err)
     }
 })
 
-//update event
-events.put('/:id', async (req,res) => {
-    try{
+// update event 
+events.put('/:id', async (req, res) => {
+    try {
         const updatedEvents = await Event.update(req.body, {
             where: {
-                band_id: req.params.id
+                event_id: req.params.id
             }
         })
         res.status(200).json({
             message: `Successfully updated ${updatedEvents} event(s)`
         })
-    }catch(err) {
+    } catch(err) {
         res.status(500).json(err)
     }
 })
 
-//delete event
-events.delete('/:id', async (req,res) => {
-    try{
+// delete event 
+events.delete('/:id', async (req, res) => {
+    try {
         const deletedEvents = await Event.destroy({
-            where:{
+            where: {
                 event_id: req.params.id
             }
         })
         res.status(200).json({
-            message: `Succesfully deleted ${deletedEvents} event(s)`
+            message: `Successfully deleted ${deletedEvents} event(s)`
         })
-    }catch(err) {
+    } catch(err) {
         res.status(500).json(err)
     }
 })
+
 
 module.exports = events
